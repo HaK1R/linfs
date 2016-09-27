e4163b1c48834ef6360f0f34e78481c8646e56d1
Ilia K <ki.stfu@gmail.com>
2016-09-27 18:50:40 +0300

    lib: Change the version number to 1.0

--- lib/layout/device_layout.h
+++ lib/layout/device_layout.h
@@ -21,8 +21,8 @@ class DeviceLayout {
     // ---
     char identifier[8] = {'\0', 'f', 'i', 'l', 'e', 'f', 's', '='};  // fs code
     struct __attribute__((packed)) {
-      uint8_t major = 0;
-      uint8_t minor = 1;
+      uint8_t major = 1;
+      uint8_t minor = 0;
     } version;                    // version (for backward compatibility)
     uint8_t cluster_size_log2;    // 2^n is actual cluster size
     uint8_t reserved0 = 0;        // reserved for future usage (but actually I'm
